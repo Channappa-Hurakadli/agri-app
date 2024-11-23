a98ffba521136009749a40415cc27cd6d73d5bdd
Channappa-Hurakadli <channappahurakadli8@gmail.com>
2024-11-23 23:18:34 +0530

    login and register added

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,68 +1,86 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom'
 
-const LoginPage = () => {
-  return (
-    <>
-      <section className="h-100 gradient-form" style="background-color: #eee;">
-        <div className="container py-5 h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-xl-10">
-              <div className="card rounded-3 text-black">
-                <div className="row g-0">
-                  <div className="col-lg-6">
-                    <div className="card-body p-md-5 mx-md-4">
+function LoginPage() {
+    return (
+      <div>
 
-                      <div className="text-center">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                           alt="logo" />
-                        <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+      {/* <!-- Section: Design Block --> */}
+      <section className="background-radial-gradient overflow-hidden">
+          <style>
+              {`
+                  .container{
+                      background-image:url("https://img.freepik.com/premium-photo/tree-with-soil-growing-converging-point-computer-circuit-board-blue-light-wireframe-network-background-green-computing-green-technology_35691-13172.jpg");
+                      background-size: cover;
+                      background-repeat: no-repeat;
+                      height:100vh;
+                      }
+
+                  .bg-glass {
+                      background-color: hsla(0, 0%, 100%, 0.5) !important; 
+                      backdrop-filter: saturate(200%) blur(25px);
+                  }
+                  
+}
+              `}
+          </style>
+
+          <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+              <div className="row gx-lg-5 align-items-center mb-5">
+                  <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
+                      <h1 className="my-5 display-5 fw-bold ls-tight" style={{ color: 'hsl(218, 81%, 95%)' }}>
+                          The best offer <br />
+                          <span style={{ color: 'hsl(218, 81%, 75%)' }}>for your business</span>
+                      </h1>
+                      <p className="mb-4 opacity-70" style={{ color: 'hsl(218, 81%, 85%)' }}>
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                          Temporibus, expedita iusto veniam atque, magni tempora mollitia
+                          dolorum consequatur nulla, neque debitis eos reprehenderit quasi
+                          ab ipsum nisi dolorem modi. Quos?
+                      </p>
+                  </div>
+
+                  <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
+                      <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+                      <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+
+                      <div className="card bg-glass">
+                          <div className="card-body px-4 py-5 px-md-5">
+                              <form>
+                                  {/* <!-- Email input --> */}
+                                  <div data-mdb-input-init className="form-outline mb-4">
+                                      <input type="email" id="form3Example3" className="form-control" />
+                                      <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                  </div>
+
+                                  {/* <!-- Password input --> */}
+                                  <div data-mdb-input-init className="form-outline mb-4">
+                                      <input type="password" id="form3Example4" className="form-control" />
+                                      <label className="form-label" htmlFor="form3Example4">Password</label>
+                                  </div>
+
+                                 
+
+                                  {/* <!-- Submit button --> */}
+                                  <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-block mb-4">
+                                      Login
+                                  </button>
+
+                                  {/* <!-- Register buttons --> */}
+                                  <div className="text-center">
+                                      <p>Don't have an account?</p>
+                                      <Link to="/signup">Sign Up</Link>
+                                  </div>
+                              </form>
+                          </div>
                       </div>
-
-                      <form>
-                        <p>Please login to your account</p>
-
-                        <div data-mdb-input-init className="form-outline mb-4">
-                          <input type="email" id="form2Example11" className="form-control"
-                            placeholder="Phone number or email address" />
-                          <label className="form-label" htmlFor="form2Example11">Username</label>
-                        </div>
-
-                        <div data-mdb-input-init className="form-outline mb-4">
-                          <input type="password" id="form2Example22" className="form-control" />
-                          <label className="form-label" htmlFor="form2Example22">Password</label>
-                        </div>
-
-                        <div className="text-center pt-1 mb-5 pb-1">
-                          <button data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                            in</button>
-                          <a className="text-muted" href="#!">Forgot password?</a>
-                        </div>
-
-                        <div className="d-flex align-items-center justify-content-center pb-4">
-                          <p className="mb-0 me-2">Don't have an account?</p>
-                          <button type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-danger">Create new</button>
-                        </div>
-
-                      </form>
-
-                    </div>
                   </div>
-                  <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-                    <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                      <h4 className="mb-4">We are more than just a company</h4>
-                      <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                  </div>
-                </div>
               </div>
-            </div>
           </div>
-        </div>
       </section>
-    </>
-  );
-};
+      {/* <!-- Section: Design Block --> */}
+  </div>
+    )
+}
 
-export default LoginPage;
+export default LoginPage
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,32 +8,32 @@ function Navbar(props) {
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary shadow bg-success-subtle">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src="https://cdn-icons-png.flaticon.com/512/500/500477.png" className="icon" alt="" /></a>
+                    <Link className="navbar-brand" to="/"><img src="https://cdn-icons-png.flaticon.com/512/500/500477.png" className="icon" alt="" /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#" onClick={(evt)=>props.handleClick(evt,"about")}>About</a>
+                                <Link className="nav-link" to="/" onClick={(evt)=>props.handleClick(evt,"about")}>About</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" onClick={(evt)=>props.handleClick(evt,"services")} href="#">Services</a>
+                                <Link className="nav-link" onClick={(evt)=>props.handleClick(evt,"services")} to="/">Services</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" onClick={(evt)=>props.handleClick(evt,"contact")} href="#">Contact</a>
+                                <Link className="nav-link" onClick={(evt)=>props.handleClick(evt,"contact")} to="/">Contact</Link>
                             </li>
                             
                         
                         </ul>
                         <ul className="d-flex gap-5 mt-2 ">
                         <a href=""><img src="/public/panelicon.webp" alt="" className='panel'/></a>
-                        <i type="button" className="fa-solid fa-language fa-lg align-content-center"></i>
                         <i type="button" className="fa-solid fa-user fa-lg align-content-center"></i>
-                        <Link to="login"><button type="button" className="btn btn-primary align-content-center">Login</button></Link>
+                        <i type="button" className="fa-solid fa-language fa-lg align-content-center"></i>
+                        <Link to="/login"><button type="button" className="btn btn-primary align-content-center">Login</button></Link>
                         </ul>
                     </div>
                 </div>
